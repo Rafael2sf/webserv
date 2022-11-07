a38772d92f64905efc6b1fffdbfbc65d9474f5fd
Dario Almeida barroca <daalmeid@c1r19s2.42lisboa.com>
2022-11-07 17:46:37 +0000

    Added documentation to Epoll class

--- srcs/core/HTTPEpoll.hpp
+++ srcs/core/HTTPEpoll.hpp
@@ -6,7 +6,7 @@
 /*   By: daalmeid <daalmeid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 14:16:23 by daalmeid          #+#    #+#             */
-/*   Updated: 2022/11/07 14:59:58 by daalmeid         ###   ########.fr       */
+/*   Updated: 2022/11/07 17:29:58 by daalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,47 @@ namespace ft {
 
 			/*Methods*/
 
+			
+			/**
+			 * @brief Creates an epoll instance to monitor incoming connections.
+			 * @param socks Contains the opened sockets where connections will come
+			 * from.
+			 * @return
+			 * On sucess, returns 0 and all opened sockets will be monitored by the
+			 * struct "events" present in this object, otherwise, -1 and
+			 * errno is set to indicate the error.
+			*/
 			int	init(HTTPSocks const& socks);
-			int insert( int __sofd);
+			
+			/**
+			 * @brief Inserts an incoming event fd in the "events" struct, allowing
+			 * its use to read/write. Flags are set appropriately.
+			 * @param sofd file descriptor of the socket from wich the event is coming from.
+			 * @return
+			 * On sucess, returns 0 and the event fd is added to the list of fds to be
+			 * monitored, otherwise, -1 and
+			 * errno is set to indicate the error.
+			*/
+			int insert( int sofd);
+			
+			/**
+			 * @brief Erases an event fd from the "events" struct and closes it.
+			 * This function should be called after the event has been handled.
+			 * @param ev_index index of the fd of the event to be deleted. This index
+			 * corresponds to a position in the internal array of the struct "events".
+			 * @return
+			 * On sucess, returns 0 and the event fd is removed and closed,
+			 *  otherwise, -1 and errno is set to indicate the error.
+			*/
 			int	erase(int ev_index);
+
+			/**
+			 * @brief Abstracts the epoll_wait() function, which waits for
+			 * incoming events through the defined fds of interest.
+			 * @return
+			 * On sucess, returns the amount of events waiting for processing,
+			 * otherwise, -1 and errno is set to indicate the error.
+			*/
 			int	wait(void);
 
 			struct epoll_event	events[EPOLL_SIZE];
